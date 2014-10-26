70c3f2af9975f5e57b8f8abc6290bed8d839bef9
jwyan1126 <jwyan1126@EP43-S3L.(none)>
2014-10-26 11:12:11 +0800

    Oct.26 2014 First Commit

--- pre_proc/mesh.c
+++ pre_proc/mesh.c
@@ -1,5 +1,7 @@
 #include"mesh.h"
 
+double pos_cal(size_t rz_s, size_t rz, const double *rz_span, const size_t *rz_subdiv);
+
 MESH *mesh_create(const SCONF *sconf)
 {
 	size_t eg_size = sconf->eg_size;
@@ -14,84 +16,32 @@ MESH *mesh_create(const SCONF *sconf)
 	// Index by [z,y,x,g,g']
 	mesh->mtrl_id = malloc(zm_mesh_size * sizeof(int **));
 	mesh->check = malloc(zm_mesh_size * sizeof(int **));
-	mesh->dx = malloc(zm_mesh_size * sizeof(double **));
-	mesh->dy = malloc(zm_mesh_size * sizeof(double **));
-	mesh->dz = malloc(zm_mesh_size * sizeof(double **));
-	mesh->xpos = malloc(zm_mesh_size * sizeof(double **));
-	mesh->ypos = malloc(zm_mesh_size * sizeof(double **));
-	mesh->zpos = malloc(zm_mesh_size * sizeof(double **));
-	mesh->chi = malloc(zm_mesh_size * sizeof(double ***));
-	mesh->dcoef = malloc(zm_mesh_size * sizeof(double ***));
-	mesh->sa = malloc(zm_mesh_size * sizeof(double ***));
-	mesh->sr = malloc(zm_mesh_size * sizeof(double ***));
-	mesh->vsf = malloc(zm_mesh_size * sizeof(double ***));
-	mesh->ss = malloc(zm_mesh_size * sizeof(double ****));
-	mesh->adfxl = malloc(zm_mesh_size * sizeof(double ***));
-	mesh->adfxr = malloc(zm_mesh_size * sizeof(double ***));
-	mesh->adfyl = malloc(zm_mesh_size * sizeof(double ***));
-	mesh->adfyr = malloc(zm_mesh_size * sizeof(double ***));
-	mesh->adfzl = malloc(zm_mesh_size * sizeof(double ***));
-	mesh->adfzr = malloc(zm_mesh_size * sizeof(double ***));
 	for(size_t k=0; k < zm_mesh_size; ++k){
 		mesh->mtrl_id[k] = malloc(ym_mesh_size * sizeof(int *));
 		mesh->check[k] = malloc(ym_mesh_size * sizeof(int *));
-		mesh->dx[k] = malloc(ym_mesh_size * sizeof(double *));
-		mesh->dy[k] = malloc(ym_mesh_size * sizeof(double *));
-		mesh->dz[k] = malloc(ym_mesh_size * sizeof(double *));
-		mesh->xpos[k] = malloc(ym_mesh_size * sizeof(double *));
-		mesh->ypos[k] = malloc(ym_mesh_size * sizeof(double *));
-		mesh->zpos[k] = malloc(ym_mesh_size * sizeof(double *));
-		mesh->chi[k] = malloc(ym_mesh_size * sizeof(double **));
-		mesh->dcoef[k] = malloc(ym_mesh_size * sizeof(double **));
-		mesh->sa[k] = malloc(ym_mesh_size * sizeof(double **));
-		mesh->sr[k] = malloc(ym_mesh_size * sizeof(double **));
-		mesh->vsf[k] = malloc(ym_mesh_size * sizeof(double **));
-		mesh->ss[k] = malloc(ym_mesh_size * sizeof(double ***));
-		mesh->adfxl[k] = malloc(ym_mesh_size * sizeof(double **));
-		mesh->adfxr[k] = malloc(ym_mesh_size * sizeof(double **));
-		mesh->adfyl[k] = malloc(ym_mesh_size * sizeof(double **));
-		mesh->adfyr[k] = malloc(ym_mesh_size * sizeof(double **));
-		mesh->adfzl[k] = malloc(ym_mesh_size * sizeof(double **));
-		mesh->adfzr[k] = malloc(ym_mesh_size * sizeof(double **));
 		for(size_t j=0; j < ym_mesh_size; ++j){
 			mesh->mtrl_id[k][j] = calloc(xm_mesh_size, sizeof(int));
 			mesh->check[k][j] = calloc(xm_mesh_size, sizeof(int));
-			mesh->dx[k][j] = calloc(xm_mesh_size, sizeof(double));
-			mesh->dy[k][j] = calloc(xm_mesh_size, sizeof(double));
-			mesh->dz[k][j] = calloc(xm_mesh_size, sizeof(double));
-			mesh->xpos[k][j] = calloc(xm_mesh_size, sizeof(double));
-			mesh->ypos[k][j] = calloc(xm_mesh_size, sizeof(double));
-			mesh->zpos[k][j] = calloc(xm_mesh_size, sizeof(double));
-			mesh->chi[k][j] = malloc(xm_mesh_size * sizeof(double *));
-			mesh->dcoef[k][j] = malloc(xm_mesh_size * sizeof(double *));
-			mesh->sa[k][j] = malloc(xm_mesh_size * sizeof(double *));
-			mesh->sr[k][j] = malloc(xm_mesh_size * sizeof(double *));
-			mesh->vsf[k][j] = malloc(xm_mesh_size * sizeof(double *));
-			mesh->ss[k][j] = malloc(xm_mesh_size * sizeof(double **));
-			mesh->adfxl[k][j] = malloc(xm_mesh_size * sizeof(double *));
-			mesh->adfxr[k][j] = malloc(xm_mesh_size * sizeof(double *));
-			mesh->adfyl[k][j] = malloc(xm_mesh_size * sizeof(double *));
-			mesh->adfyr[k][j] = malloc(xm_mesh_size * sizeof(double *));
-			mesh->adfzl[k][j] = malloc(xm_mesh_size * sizeof(double *));
-			mesh->adfzr[k][j] = malloc(xm_mesh_size * sizeof(double *));
-			for(size_t g=0; g< eg_size; ++g){
-				mesh->chi[k][j][i] = calloc(eg_size, sizeof(double));
-				mesh->dcoef[k][j][i] = calloc(eg_size, sizeof(double));
-				mesh->sa[k][j][i] = calloc(eg_size, sizeof(double));
-				mesh->sr[k][j][i] = calloc(eg_size, sizeof(double));
-				mesh->vsf[k][j][i] = calloc(eg_size, sizeof(double));
-				mesh->ss[k][j][i] = malloc(eg_size * sizeof(double *));
-				mesh->adfxl[k][j][i] = calloc(eg_size, sizeof(double));
-				mesh->adfxr[k][j][i] = calloc(eg_size, sizeof(double));
-				mesh->adfyl[k][j][i] = calloc(eg_size, sizeof(double));
-				mesh->adfyr[k][j][i] = calloc(eg_size, sizeof(double));
-				mesh->adfzl[k][j][i] = calloc(eg_size, sizeof(double));
-				mesh->adfzr[k][j][i] = calloc(eg_size, sizeof(double));
-				for(size_t from_g=0; from_g< eg_size; ++from_g)
-					mesh->ss[k][j][i][g] = calloc(eg_size, sizeof(double));
-			}
 		}
 	}
+	mesh->dx = cdat3_create(xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->dy = cdat3_create(xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->dz = cdat3_create(xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->xpos = cdat3_create(xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->ypos = cdat3_create(xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->zpos = cdat3_create(xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->chi = cdat4_create(eg_size, xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->dcoef = cdat4_create(eg_size, xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->sa = cdat4_create(eg_size, xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->sr = cdat4_create(eg_size, xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->vsf = cdat4_create(eg_size, xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->ss = cdat5_create(eg_size, xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->adfxl = cdat4_create(eg_size, xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->adfxr = cdat4_create(eg_size, xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->adfyl = cdat4_create(eg_size, xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->adfyr = cdat4_create(eg_size, xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->adfzl = cdat4_create(eg_size, xm_mesh_size, ym_mesh_size, zm_mesh_size);
+	mesh->adfzr = cdat4_create(eg_size, xm_mesh_size, ym_mesh_size, zm_mesh_size);
 	
 	size_t xm_span_size = sconf->xm_span_size;
 	size_t ym_span_size = sconf->ym_span_size;
@@ -104,10 +54,40 @@ MESH *mesh_create(const SCONF *sconf)
 	double *zspan_len = sconf->zspan_len;
 	int ***mtrl_set = sconf->mtrl_set;
 	MTRLLIB *mlib = sconf->mtrllib;
-	for(size_t k=0; k<zm_span_size; ++k)
-		for(size_t j=0; j<ym_span_size; ++j)
-			for(size_t i=0; i<xm_span_size; ++i){
-				MBLOCK //////
+	for(size_t zspan=0; zspan<zm_span_size; ++zspan)
+		for(size_t yspan=0; yspan<ym_span_size; ++yspan)
+			for(size_t xspan=0; xspan<xm_span_size; ++xspan){
+				MBLOCK mblock = sconf_get_mblock(sconf, xspan, yspan, zspan);
+				for(size_t k = mblock.start_z; k <= mblock.end_z; ++k)
+					for(size_t j = mblock.start_y; j <= mblock.end_y; ++j)
+						for(size_t i = mblock.start_x; i <= mblock.end_x; ++i){
+							mesh->check[k][j][i] = 1;
+							int mtrl_id = mtrl_set[xspan][yspan][zspan];
+							MTRL *m = mtrllib_get_fromid(mlib, mtrl_id);
+							mesh->mtrl_id[k][j][i] = mtrl_id;
+							cdat3_set_val(mesh->dx, i, j, k, xspan_len[xspan]/xspan_subdiv[xspan]);
+							cdat3_set_val(mesh->dy, i, j, k, yspan_len[yspan]/yspan_subdiv[yspan]);
+							cdat3_set_val(mesh->dz, i, j, k, zspan_len[zspan]/zspan_subdiv[zspan]);
+							cdat3_set_val(mesh->xpos, i, j, k, pos_cal(xspan, i, xspan_len, xspan_subdiv));
+							cdat3_set_val(mesh->ypos, i, j, k, pos_cal(yspan, j, yspan_len, yspan_subdiv));
+							cdat3_set_val(mesh->zpos, i, j, k, pos_cal(zspan, k, zspan_len, zspan_subdiv));
+							for(size_t g=0; g< eg_size; ++g){
+								cdat4_set_val(mesh->chi, g, i, j, k, m->chi[g]);
+								cdat4_set_val(mesh->dcoef, g, i, j, k, m->dcoef[g]);
+								cdat4_set_val(mesh->sa, g, i, j, k, m->sa[g]);
+								cdat4_set_val(mesh->sr, g, i, j, k, m->sr[g]);
+								cdat4_set_val(mesh->vsf, g, i, j, k, m->vsf[g]);
+								for(size_t from_g=0; from_g< eg_size; ++from_g)
+									cdat5_set_val(mesh->ss, g, from_g, i, j, k, m->ss[g][from_g]);
+								// ADFs
+								cdat4_set_val(mesh->adfxl, g, i, j, k, (i == mblock.start_x) ? m->adfxl[g] : 1.0);
+								cdat4_set_val(mesh->adfxr, g, i, j, k, (i == mblock.end_x) ? m->adfxr[g] : 1.0);
+								cdat4_set_val(mesh->adfyl, g, i, j, k, (j == mblock.start_y) ? m->adfyl[g] : 1.0);
+								cdat4_set_val(mesh->adfyr, g, i, j, k, (j == mblock.end_y) ? m->adfyr[g] : 1.0);
+								cdat4_set_val(mesh->adfzl, g, i, j, k, (k == mblock.start_z) ? m->adfzl[g] : 1.0);
+								cdat4_set_val(mesh->adfzr, g, i, j, k, (k == mblock.end_y) ? m->adfzr[g] : 1.0);
+							}
+						}
 			}
 	return mesh;
 }
@@ -116,82 +96,14 @@ void mesh_free(MESH *mesh)
 {
 	for(size_t k=0; k< zm_mesh_size; ++k){
 		for(size_t j=0; j< ym_mesh_size; ++j){
-			for(size_t i=0; i< xm_mesh_size; ++i){
-				for(size_t g=0; g< eg_size; ++g)
-					free(mesh->ss[k][j][i][g]);
-				free(mesh->chi[k][j][i]);
-				free(mesh->dcoef[k][j][i]);
-				free(mesh->sa[k][j][i]);
-				free(mesh->sr[k][j][i]);
-				free(mesh->vsf[k][j][i]);
-				free(mesh->ss[k][j][i]);
-				free(mesh->adfxl[k][j][i]);
-				free(mesh->adfxr[k][j][i]);
-				free(mesh->adfyl[k][j][i]);
-				free(mesh->adfyr[k][j][i]);
-				free(mesh->adfzl[k][j][i]);
-				free(mesh->adfzr[k][j][i]);
-			}
 			free(mesh->mtrl_id[k][j]);
 			free(mesh->check[k][j]);
-			free(mesh->dx[k][j]);
-			free(mesh->dy[k][j]);
-			free(mesh->dz[k][j]);
-			free(mesh->xpos[k][j]);
-			free(mesh->ypos[k][j]);
-			free(mesh->zpos[k][j]);
-			free(mesh->chi[k][j]);
-			free(mesh->dcoef[k][j]);
-			free(mesh->sa[k][j]);
-			free(mesh->sr[k][j]);
-			free(mesh->vsf[k][j]);
-			free(mesh->ss[k][j]);
-			free(mesh->adfxl[k][j]);
-			free(mesh->adfxr[k][j]);
-			free(mesh->adfyl[k][j]);
-			free(mesh->adfyr[k][j]);
-			free(mesh->adfzl[k][j]);
-			free(mesh->adfzr[k][j]);
 		}
 		free(mesh->mtrl_id[k]);
-		free(mesh->dx[k]);
-		free(mesh->dy[k]);
-		free(mesh->dz[k]);
-		free(mesh->xpos[k]);
-		free(mesh->ypos[k]);
-		free(mesh->zpos[k]);
-		free(mesh->chi[k]);
-		free(mesh->dcoef[k]);
-		free(mesh->sa[k]);
-		free(mesh->sr[k]);
-		free(mesh->vsf[k]);
-		free(mesh->ss[k]);
-		free(mesh->adfxl[k]);
-		free(mesh->adfxr[k]);
-		free(mesh->adfyl[k]);
-		free(mesh->adfyr[k]);
-		free(mesh->adfzl[k]);
-		free(mesh->adfzr[k]);
+		free(mesh->check[k]);
 	}
 	free(mesh->mtrl_id);
-	free(mesh->dx);
-	free(mesh->dy);
-	free(mesh->dz);
-	free(mesh->xpos);
-	free(mesh->ypos);
-	free(mesh->zpos);
-	free(mesh->chi);
-	free(mesh->dcoef);
-	free(mesh->sa);
-	free(mesh->sr);
-	free(mesh->vsf);
-	free(mesh->ss);
-	free(mesh->adfxl);
-	free(mesh->adfxr);
-	free(mesh->adfyl);
-	free(mesh->adfyr);
-	free(mesh->adfzl);
-	free(mesh->adfzr);
+	free(mesh->check);
 }
 
 inline int mesh_get_mtrl_id(const MESH *mesh, size_t i, size_t j, size_t k)
@@ -213,7 +125,7 @@ inline double mesh_get_dx(const MESH *mesh, size_t i, size_t j, size_t k)
 		exit(-1);
 	}
 	#endif
-	return(mesh->dx[k][j][i]);
+	return cdat3_get_val(mesh->dx, i, j, k);
 }
 
 inline double mesh_get_dy(const MESH *mesh, size_t i, size_t j, size_t k)
@@ -224,7 +136,7 @@ inline double mesh_get_dy(const MESH *mesh, size_t i, size_t j, size_t k)
 		exit(-1);
 	}
 	#endif
-	return(mesh->dy[k][j][i]);
+	return cdat3_get_val(mesh->dy, i, j, k);
 }
 
 inline double mesh_get_dz(const MESH *mesh, size_t i, size_t j, size_t k)
@@ -235,7 +147,7 @@ inline double mesh_get_dz(const MESH *mesh, size_t i, size_t j, size_t k)
 		exit(-1);
 	}
 	#endif
-	return(mesh->dz[k][j][i]);
+	return cdat3_get_val(mesh->dy, i, j, k);
 }
 
 inline double mesh_get_xpos(const MESH *mesh, size_t i, size_t j, size_t k)
@@ -246,7 +158,7 @@ inline double mesh_get_xpos(const MESH *mesh, size_t i, size_t j, size_t k)
 		exit(-1);
 	}
 	#endif
-	return(mesh->xpos[k][j][i]);
+	return cdat3_get_val(mesh->xpos, i, j, k);
 }
 
 inline double mesh_get_ypos(const MESH *mesh, size_t i, size_t j, size_t k)
@@ -257,7 +169,7 @@ inline double mesh_get_ypos(const MESH *mesh, size_t i, size_t j, size_t k)
 		exit(-1);
 	}
 	#endif
-	return(mesh->ypos[k][j][i]);
+	return cdat3_get_val(mesh->ypos, i, j, k);
 }
 
 inline double mesh_get_zpos(const MESH *mesh, size_t i, size_t j, size_t k)
@@ -268,7 +180,7 @@ inline double mesh_get_zpos(const MESH *mesh, size_t i, size_t j, size_t k)
 		exit(-1);
 	}
 	#endif
-	return(mesh->zpos[k][j][i]);
+	return cdat3_get_val(mesh->zpos, i, j, k);
 }
 
 inline double mesh_get_chi(const MESH *mesh, size_t g, size_t i, size_t j, size_t k)
@@ -279,7 +191,7 @@ inline double mesh_get_chi(const MESH *mesh, size_t g, size_t i, size_t j, size_
 		exit(-1);
 	}
 	#endif
-	return(mesh->chi[k][j][i][g]);
+	return cdat4_get_val(mesh->chi, g, i, j, k);
 }
 
 inline double mesh_get_dcoef(const MESH *mesh, size_t g, size_t i, size_t j, size_t k)
@@ -290,7 +202,7 @@ inline double mesh_get_dcoef(const MESH *mesh, size_t g, size_t i, size_t j, siz
 		exit(-1);
 	}
 	#endif
-	return(mesh->dcoef[k][j][i][g]);
+	return cdat4_get_val(mesh->dcoef, g, i, j, k);
 }
 
 inline double mesh_get_sa(const MESH *mesh, size_t g, size_t i, size_t j, size_t k)
@@ -301,7 +213,7 @@ inline double mesh_get_sa(const MESH *mesh, size_t g, size_t i, size_t j, size_t
 		exit(-1);
 	}
 	#endif
-	return(mesh->sa[k][j][i][g]);
+	return cdat4_get_val(mesh->sa, g, i, j, k);
 }
 
 inline double mesh_get_sr(const MESH *mesh, size_t g, size_t i, size_t j, size_t k)
@@ -312,7 +224,7 @@ inline double mesh_get_sr(const MESH *mesh, size_t g, size_t i, size_t j, size_t
 		exit(-1);
 	}
 	#endif
-	return(mesh->sr[k][j][i][g]);
+	return cdat4_get_val(mesh->sr, g, i, j, k);
 }
 
 inline double mesh_get_vsf(const MESH *mesh, size_t g, size_t i, size_t j, size_t k)
@@ -323,7 +235,7 @@ inline double mesh_get_vsf(const MESH *mesh, size_t g, size_t i, size_t j, size_
 		exit(-1);
 	}
 	#endif
-	return(mesh->vsf[k][j][i][g]);
+	return cdat4_get_val(mesh->vsf, g, i, j, k);
 }
 
 inline double mesh_get_ss(const MESH *mesh, size_t g, size_t from_g, size_t i, size_t j, size_t k)
@@ -334,7 +246,7 @@ inline double mesh_get_ss(const MESH *mesh, size_t g, size_t from_g, size_t i, s
 		exit(-1);
 	}
 	#endif
-	return(mesh->ss[k][j][i][g][from_g]);
+	return cdat5_get_val(mesh->ss, g, from_g, i, j, k);
 }
 
 inline double mesh_get_adfxl(const MESH *mesh, size_t g, size_t i, size_t j, size_t k)
@@ -345,7 +257,7 @@ inline double mesh_get_adfxl(const MESH *mesh, size_t g, size_t i, size_t j, siz
 		exit(-1);
 	}
 	#endif
-	return(mesh->adfxl[k][j][i][g]);
+	return cdat4_get_val(mesh->adfxl, g, i, j, k);
 }
 
 inline double mesh_get_adfxr(const MESH *mesh, size_t g, size_t i, size_t j, size_t k)
@@ -356,7 +268,7 @@ inline double mesh_get_adfxr(const MESH *mesh, size_t g, size_t i, size_t j, siz
 		exit(-1);
 	}
 	#endif
-	return(mesh->adfxr[k][j][i][g]);
+	return cdat4_get_val(mesh->adfxr, g, i, j, k);
 }
 
 inline double mesh_get_adfyl(const MESH *mesh, size_t g, size_t i, size_t j, size_t k)
@@ -367,7 +279,7 @@ inline double mesh_get_adfyl(const MESH *mesh, size_t g, size_t i, size_t j, siz
 		exit(-1);
 	}
 	#endif
-	return(mesh->adfyl[k][j][i][g]);
+	return cdat4_get_val(mesh->adfyl, g, i, j, k);
 }
 
 inline double mesh_get_adfyr(const MESH *mesh, size_t g, size_t i, size_t j, size_t k)
@@ -378,7 +290,7 @@ inline double mesh_get_adfyr(const MESH *mesh, size_t g, size_t i, size_t j, siz
 		exit(-1);
 	}
 	#endif
-	return(mesh->adfyr[k][j][i][g]);
+	return cdat4_get_val(mesh->adfyr, g, i, j, k);
 }
 
 inline double mesh_get_adfzl(const MESH *mesh, size_t g, size_t i, size_t j, size_t k)
@@ -389,7 +301,7 @@ inline double mesh_get_adfzl(const MESH *mesh, size_t g, size_t i, size_t j, siz
 		exit(-1);
 	}
 	#endif
-	return(mesh->adfzl[k][j][i][g]);
+	return cdat4_get_val(mesh->adfzl, g, i, j, k);
 }
 
 inline double mesh_get_adfzr(const MESH *mesh, size_t g, size_t i, size_t j, size_t k)
@@ -400,5 +312,20 @@ inline double mesh_get_adfzr(const MESH *mesh, size_t g, size_t i, size_t j, siz
 		exit(-1);
 	}
 	#endif
-	return(mesh->adfzr[k][j][i][g]);
+	return cdat4_get_val(mesh->adfzr, g, i, j, k);
+}
+
+// private func.
+double pos_cal(size_t rz_s, size_t rz, const double *rz_span, const size_t *rz_subdiv)
+{
+	double tmp = 0.0;
+	double t = rz;
+	for(size_t i=0; i< rz_s; ++i){
+		tmp += rz_span[i];
+		t -= rz_subdiv[i];
+	}
+	double d = rz_span[rz_s] / rz_subdiv[rz_s];
+	tmp += t * d;
+	tmp += d/2.0;
+	return tmp;
 }
--- pre_proc/mesh.h
+++ pre_proc/mesh.h
@@ -3,33 +3,34 @@
 
 #include<stddef.h>
 #include<sconf.h>
+#include"cdat.h"
 
 typedef struct
 {
 	size_t eg_size;
-	size_t xm_size;
-	size_t ym_size;
-	size_t zm_size;
-	int ***check;
-	int ***mtrl_id;
-	double ***dx;
-	double ***dy;
-	double ***dz;
-	double ***xpos;
-	double ***ypos;
-	double ***zpos;
-	double ****chi;
-	double ****dcoef;
-	double ****sa;
-	double ****sr;
-	double ****vsf;
-	double *****ss;
-	double ****adfxl;
-	double ****adfxr;
-	double ****adfyl;
-	double ****adfyr;
-	double ****adfzl;
-	double ****adfzr;
+	size_t xm_mesh_size;
+	size_t ym_mesh_size;
+	size_t zm_mesh_size;
+	int ***check; // indexed by [z][y][x]
+	int ***mtrl_id; // Like above
+	CDAT3 *dx;
+	CDAT3 *dy;
+	CDAT3 *dz;
+	CDAT3 *xpos;
+	CDAT3 *ypos;
+	CDAT3 *zpos;
+	CDAT4 *chi;
+	CDAT4 *dcoef;
+	CDAT4 *sa;
+	CDAT4 *sr;
+	CDAT4 *vsf;
+	CDAT5 *ss;
+	CDAT4 *adfxl;
+	CDAT4 *adfxr;
+	CDAT4 *adfyl;
+	CDAT4 *adfyr;
+	CDAT4 *adfzl;
+	CDAT4 *adfzr;
 } MESH;
 
 MESH *mesh_create(const SCONF *sconf);
