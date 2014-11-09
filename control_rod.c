06c8003f0fa03ae3c6536d540461d426c506ef56
jwyan1126 <jwyan1126@localhost.localdomain>
2014-11-08 20:35:40 -0500

    2D TWIGL Benchmark test successfully

--- control_rod.c
+++ control_rod.c
@@ -2,7 +2,7 @@
 
 void cross_section_update(MESH *mesh, double cur_time, SCONF *sconf)
 {
-	MBLOCK mblock = sconf_get_mblock(sconf, 0,0,0);
+	MBLOCK mblock = sconf_get_mblock(sconf, 1,1,0);
 	size_t start_x = mblock.start_x;
 	size_t end_x = mblock.end_x;
 	size_t start_y = mblock.start_y;
@@ -12,6 +12,9 @@ void cross_section_update(MESH *mesh, double cur_time, SCONF *sconf)
 	for(size_t i=start_x; i<=end_x; ++i)
 		for(size_t j=start_y; j<=end_y; ++j)
 			for(size_t k=start_z; k<=end_z; ++k){
-				mesh->sr->data[k][j][i][1] = 0.18*(1.0-0.01*cur_time);
+				if(cur_time <=0.2)
+					mesh->sr->data[k][j][i][1] = 0.15*(1.0-0.11667*cur_time);
+				else
+					mesh->sr->data[k][j][i][1] = 0.15*0.97666;
 			}
 }
